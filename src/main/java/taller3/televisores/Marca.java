f9a4191ec24fbe579a004898cbb5beb00876fcc6
JulianMart2706 <julmartinezc@unal.edu.co>
2024-02-26 21:09:46 -0500

    Commit Julian

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,15 @@
+public class Marca{
+    private String nombre;
+
+    public Marca(String nombre){
+        this.setnombre(nombre);
+    }
+
+    public String getnombre(){
+        return this.nombre
+    }
+
+    public void setnombre(String nombre){
+        this.nombre = nombre;
+    }
+}
